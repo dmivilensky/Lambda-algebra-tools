3cd083eb2b6ad293ebef61a2e9a1efcf874ac6c3
dmivilensky <pasechnyuk2004@gmail.com>
2023-05-12 22:01:41 +0300

    embedding into free lie algebra

--- lambda.hs
+++ lambda.hs
@@ -48,12 +48,46 @@ equivalence (Sum as) = Sum (map equivalence as)
 
 lambda_1_lambda_1 = equivalence (lambda_1 <$> lambda_1 (SphereSpectrum 4 []))
 lambda_2_lambda_1 = equivalence (lambda_1 <$> lambda_2 (SphereSpectrum 4 []))
-lambda_1_lambda_1_lambda_1 = lambda_1 <$> equivalence (lambda_1 <$> lambda_1 (SphereSpectrum 8 []))
+lambda_1_lambda_1_lambda_1 = equivalence (lambda_1 <$> equivalence (lambda_1 <$> lambda_1 (SphereSpectrum 8 [])))
+
+data FreeLie = FreeUnit Int | FreeCommutator FreeLie FreeLie | FreeSum [FreeLie]
+
+instance Show FreeLie where
+    show (FreeUnit a) = case a of
+        0 -> "x"
+        1 -> "y"
+        2 -> "z"
+        3 -> "p"
+        4 -> "q"
+        5 -> "u"
+        6 -> "v"
+        7 -> "w"
+    show (FreeCommutator a b) = "[" ++ show a ++ ", " ++ show b ++ "]"
+    show (FreeSum a) = intercalate " + " (map show a)
+
+computeSuspension i [j] = if j <= i then i + 1 else i
+computeSuspension i s   = computeSuspension (computeSuspension i (tail s)) [head s]
+
+computeSuspensionLie (Unit (SphereSpectrum i s)) = FreeUnit (computeSuspension (i-1) s)
+computeSuspensionLie (Commutator a b) = FreeCommutator (computeSuspensionLie a) (computeSuspensionLie b)
+computeSuspensionLie (Sum as) = FreeSum (map computeSuspensionLie as)
 
 main = do
-    putStrLn "λ₁λ₁ = "
+    putStr "λ₁     = "
+    print $ lambda_1 (SphereSpectrum 2 [])
+    putStr "λ₁λ₁   = "
     print lambda_1_lambda_1
-    putStrLn "λ₂λ₁ = "
+    putStr "λ₂λ₁   = "
     print lambda_2_lambda_1
-    putStrLn "λ₁λ₁λ₁ = "
+    putStr "λ₁λ₁λ₁ = "
     print lambda_1_lambda_1_lambda_1
+    putStrLn ""
+
+    putStr "λ₁     = "
+    print $ computeSuspensionLie $ lambda_1 (SphereSpectrum 2 [])
+    putStr "λ₁λ₁   = "
+    print $ computeSuspensionLie lambda_1_lambda_1
+    putStr "λ₂λ₁   = "
+    print $ computeSuspensionLie lambda_2_lambda_1
+    putStr "λ₁λ₁λ₁ = "
+    print $ computeSuspensionLie lambda_1_lambda_1_lambda_1
